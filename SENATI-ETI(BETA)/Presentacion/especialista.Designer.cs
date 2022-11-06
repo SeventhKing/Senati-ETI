ff0d3efdad6d31f9afe7a5be0b8bdfc8eb4239a0
Seven <yetmoja16@gmail.com>
2022-11-05 19:21:37 -0500

    mostrar tabla en menu principal

--- SENATI-ETI(BETA)/Presentacion/especialista.Designer.cs
+++ SENATI-ETI(BETA)/Presentacion/especialista.Designer.cs
@@ -29,42 +29,71 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(especialista));
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.btnAlumno = new System.Windows.Forms.Button();
-            this.btnEmpresa = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.panelMenu = new System.Windows.Forms.Panel();
             this.btnLogOut = new System.Windows.Forms.Button();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
+            this.btnConvenio = new System.Windows.Forms.Button();
+            this.btnEmpresa = new System.Windows.Forms.Button();
+            this.btnAlumno = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panelMuestra = new System.Windows.Forms.Panel();
+            this.panelMenu.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // panel1
+            // panelMenu
             // 
-            this.panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.panel1.Controls.Add(this.panel3);
-            this.panel1.Controls.Add(this.panel2);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1172, 640);
-            this.panel1.TabIndex = 0;
+            this.panelMenu.Controls.Add(this.btnLogOut);
+            this.panelMenu.Controls.Add(this.btnConvenio);
+            this.panelMenu.Controls.Add(this.btnEmpresa);
+            this.panelMenu.Controls.Add(this.btnAlumno);
+            this.panelMenu.Controls.Add(this.pictureBox1);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(292, 640);
+            this.panelMenu.TabIndex = 0;
             // 
-            // panel2
+            // btnLogOut
             // 
-            this.panel2.Controls.Add(this.btnLogOut);
-            this.panel2.Controls.Add(this.button2);
-            this.panel2.Controls.Add(this.btnEmpresa);
-            this.panel2.Controls.Add(this.btnAlumno);
-            this.panel2.Controls.Add(this.pictureBox1);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel2.Location = new System.Drawing.Point(0, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(239, 640);
-            this.panel2.TabIndex = 0;
+            this.btnLogOut.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnLogOut.Location = new System.Drawing.Point(0, 597);
+            this.btnLogOut.Name = "btnLogOut";
+            this.btnLogOut.Size = new System.Drawing.Size(292, 43);
+            this.btnLogOut.TabIndex = 4;
+            this.btnLogOut.Text = "Cerrar Sesion";
+            this.btnLogOut.UseVisualStyleBackColor = true;
+            // 
+            // btnConvenio
+            // 
+            this.btnConvenio.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnConvenio.Location = new System.Drawing.Point(0, 286);
+            this.btnConvenio.Name = "btnConvenio";
+            this.btnConvenio.Size = new System.Drawing.Size(292, 47);
+            this.btnConvenio.TabIndex = 3;
+            this.btnConvenio.Text = "Convenio";
+            this.btnConvenio.UseVisualStyleBackColor = true;
+            // 
+            // btnEmpresa
+            // 
+            this.btnEmpresa.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnEmpresa.Location = new System.Drawing.Point(0, 239);
+            this.btnEmpresa.Name = "btnEmpresa";
+            this.btnEmpresa.Size = new System.Drawing.Size(292, 47);
+            this.btnEmpresa.TabIndex = 2;
+            this.btnEmpresa.Text = "Empresa";
+            this.btnEmpresa.UseVisualStyleBackColor = true;
+            this.btnEmpresa.Click += new System.EventHandler(this.btnEmpresa_Click);
+            // 
+            // btnAlumno
+            // 
+            this.btnAlumno.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnAlumno.Location = new System.Drawing.Point(0, 192);
+            this.btnAlumno.Name = "btnAlumno";
+            this.btnAlumno.Size = new System.Drawing.Size(292, 47);
+            this.btnAlumno.TabIndex = 1;
+            this.btnAlumno.Text = "Alumno";
+            this.btnAlumno.UseVisualStyleBackColor = true;
+            this.btnAlumno.Click += new System.EventHandler(this.btnAlumno_Click);
             // 
             // pictureBox1
             // 
@@ -72,83 +101,43 @@
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(239, 192);
+            this.pictureBox1.Size = new System.Drawing.Size(292, 192);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // btnAlumno
+            // panelMuestra
             // 
-            this.btnAlumno.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btnAlumno.Location = new System.Drawing.Point(0, 192);
-            this.btnAlumno.Name = "btnAlumno";
-            this.btnAlumno.Size = new System.Drawing.Size(239, 47);
-            this.btnAlumno.TabIndex = 1;
-            this.btnAlumno.Text = "Alumno";
-            this.btnAlumno.UseVisualStyleBackColor = true;
-            // 
-            // btnEmpresa
-            // 
-            this.btnEmpresa.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btnEmpresa.Location = new System.Drawing.Point(0, 239);
-            this.btnEmpresa.Name = "btnEmpresa";
-            this.btnEmpresa.Size = new System.Drawing.Size(239, 47);
-            this.btnEmpresa.TabIndex = 2;
-            this.btnEmpresa.Text = "Empresa";
-            this.btnEmpresa.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button2.Location = new System.Drawing.Point(0, 286);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(239, 47);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Convenio";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // btnLogOut
-            // 
-            this.btnLogOut.Location = new System.Drawing.Point(0, 591);
-            this.btnLogOut.Name = "btnLogOut";
-            this.btnLogOut.Size = new System.Drawing.Size(239, 49);
-            this.btnLogOut.TabIndex = 4;
-            this.btnLogOut.Text = "Cerrar Sesion";
-            this.btnLogOut.UseVisualStyleBackColor = true;
-            // 
-            // panel3
-            // 
-            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel3.Location = new System.Drawing.Point(239, 0);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(933, 640);
-            this.panel3.TabIndex = 1;
+            this.panelMuestra.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelMuestra.Location = new System.Drawing.Point(292, 0);
+            this.panelMuestra.Name = "panelMuestra";
+            this.panelMuestra.Size = new System.Drawing.Size(880, 640);
+            this.panelMuestra.TabIndex = 1;
             // 
             // especialista
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1172, 640);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panelMuestra);
+            this.Controls.Add(this.panelMenu);
             this.Name = "especialista";
             this.Text = "Menu";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.especialista_FormClosing);
-            this.panel1.ResumeLayout(false);
-            this.panel2.ResumeLayout(false);
+            this.Load += new System.EventHandler(this.especialista_Load);
+            this.panelMenu.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Button btnConvenio;
         private System.Windows.Forms.Button btnEmpresa;
         private System.Windows.Forms.Button btnAlumno;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Button btnLogOut;
+        private System.Windows.Forms.Panel panelMuestra;
     }
 }
--- SENATI-ETI(BETA)/Presentacion/especialista.cs
+++ SENATI-ETI(BETA)/Presentacion/especialista.cs
@@ -5,7 +5,9 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
+using SENATI_ETI_BETA_.Presentacion;
 
 namespace SENATI_ETI_BETA_
 {
@@ -19,9 +21,61 @@ namespace SENATI_ETI_BETA_
 
         private void especialista_FormClosing(object sender, FormClosingEventArgs e)
         {
-            
             Login log = new Login();
             log.Show();
+        }
+
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+        private void panelMuestra_MouseMove(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void especialista_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void AbrirFormulario<MiForm>() where MiForm : Form, new()
+        {
+            Form formulario;
+            formulario = panelMuestra.Controls.OfType<MiForm>().FirstOrDefault();//Busca en la colecion el formulario
+                                                                                     //si el formulario/instancia no existe
+            if (formulario == null)
+            {
+                formulario = new MiForm();
+                formulario.TopLevel = false;
+                formulario.FormBorderStyle = FormBorderStyle.None;
+                formulario.Dock = DockStyle.Fill;
+                panelMuestra.Controls.Add(formulario);
+                panelMuestra.Tag = formulario;
+                formulario.Show();
+                formulario.BringToFront();
+            }
+            //si el formulario/instancia existe
+            else
+            {
+                formulario.BringToFront();
+            }
+        }
+
+        private void panelMuestra_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnAlumno_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void btnEmpresa_Click(object sender, EventArgs e)
+        {
+            AbrirFormulario<tabla_empresa>();
+
         }
     }
 }
